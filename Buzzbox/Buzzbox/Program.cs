16c5f49bb3775241626c24feec07d96a52f0eec2
Xesyto <s.oudenaarden@gmail.com>
2016-04-29 16:29:36 +0200

    Added commandline dependency. Added commandline options.

--- Buzzbox/Buzzbox/Program.cs
+++ Buzzbox/Buzzbox/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using System.IO;
 using Buzzbox_Common;
 using CommandLine;
@@ -8,8 +9,45 @@ namespace Buzzbox
 {
     class Program
     {
-        static void Main(string[] args)
+
+        //Command line options through CommandLine: http://commandline.codeplex.com/
+        class Options
         {
+            [Option('i', "input", 
+                Required = true, 
+                HelpText = "Path to input file to be Encoded, must be in hearthstonejson format.")]
+            public string InputFile { get; set; }
+
+            [Option('o', "output",
+                HelpText = "Output file path. Defaults to output.txt",
+                DefaultValue = "output.txt")]
+            public string OutputFile { get; set; }
+
+            [Option('e', "encoding",
+                HelpText = "Which encoding format to use.",
+                DefaultValue = EncodingFormats.scfdivineFormat)]
+            public EncodingFormats EncodingFormat { get; set; }
+
+            [HelpOption]
+            public string GetUsage()
+            {
+                return "Encodes a hearthstonejson file to a mtg-rnn usable corpus txt file. \n\n" + 
+                    HelpText.AutoBuild(this,
+                  (current) => HelpText.DefaultParsingErrorsHandler(this, current));
+            }
+        }
+
+        private static void Main(string[] args)
+        {
+            //Parse Commandline options
+            var options = new Options();
+            var commandLineResults = Parser.Default.ParseArguments(args, options);
+
+            //Only continue if commandline options fullfilled. CommandLine will handle helptext if something was off.
+            if (commandLineResults)
+            {
+                
+            }
         }
     }
 }
