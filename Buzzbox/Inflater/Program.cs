e017b297324402b29ea2b891cc8236339c79e203
Xesyto <s.oudenaarden@gmail.com>
2016-04-29 12:47:05 +0200

    Added CommandLine Package to Inflater and set up command line options.

--- Buzzbox/Inflater/Program.cs
+++ Buzzbox/Inflater/Program.cs
@@ -1,15 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CommandLine;
+using CommandLine.Text;
 
 namespace Inflater
 {
     class Program
     {
+        class Options
+        {
+            [Option('i',"input",Required = true, HelpText = "Input file to be inflated, must be in api.hearthstonejson format.")]
+            public string InputFile { get; set; }
+
+            [Option('o',"output",HelpText = "Output file. Defaults to inflated.(inputFileName)")]
+            public string OutputFile { get; set; }
+
+            [Option('r',"rate", HelpText = "Amount of inflation. Defaults to 3 times original.")]
+            public int InflationRate { get; set; } = 3;
+
+            [HelpOption]
+            public string GetUsage()
+            {
+                return HelpText.AutoBuild(this,
+                  (current) => HelpText.DefaultParsingErrorsHandler(this, current));
+            }
+        }
+
         static void Main(string[] args)
         {
+            var options = new Options();
+            var commandLineResults = Parser.Default.ParseArguments(args, options);
+
+            if (commandLineResults)
+            {
+                
+            }
         }
     }
 }
