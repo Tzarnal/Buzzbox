8bde77dbf1792350fe3e98912d5a0ab1db92c473
Xesyto <s.oudenaarden@gmail.com>
2016-05-02 19:32:19 +0200

    Added  Encode Test

---
+++ Buzzbox/Encode-Tests/EncodeTests.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Buzzbox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Buzzbox_Common;
+
+namespace Buzzbox.Tests
+{
+    [TestClass()]
+    public class EncodeTests
+    {
+        Card testMinion = new Card
+        {
+            Name = "Tirion Fordring",
+            Type = "MINION",
+            PlayerClass = "PALADIN",
+            Rarity = "LEGENDARY",
+            Cost = 8,
+            Attack = 6,
+            Health = 6,
+            Text = "<b>Divine Shield</b>. <b>Taunt</b>. <b>Deathrattle:</b> Equip a 5/3 Ashbringer."
+        };
+
+        Card testSpell = new Card
+        {
+            Name = "Fireball",
+            Type = "SPELL",
+            PlayerClass = "MAGE",
+            Rarity = "FREE",
+            Cost = 4,
+            Text = "Deal $6 damage."
+        };
+
+        Card testWeapon = new Card
+        {
+            Name = "Death's Bite",
+            Type = "WEAPON",
+            PlayerClass = "WARRIOR",
+            Rarity = "COMMON",
+            Cost = 4,
+            Attack = 4,
+            Durability = 2,
+            Text = "<b>Deathrattle:</b> Deal 1 damage to all minions."
+        };
+        
+        [TestMethod()]
+        public void EncodeCardCollectionTest()
+        {
+            var collection = new CardCollection();
+            collection.Cards.Add(testMinion);
+            collection.Cards.Add(testSpell);
+            collection.Cards.Add(testWeapon);
+
+            var encoder = new Encode();
+            var result = encoder.EncodeCardCollection(collection, EncodingFormats.scfdivineFormat);
+
+            var expected =
+                "Tirion Fordring @ Paladin |  | Minion | L | 8 | 6/6 || $DV$. $T$. $DR$ Equip a 5/3 Ashbringer. &\r\n\r\nFireball @ Mage | Spell | C | 4 || Deal $6 damage. &\r\n\r\nDeath's Bite @ Warrior | Weapon | C | 4 | 4/2 || $DR$ Deal 1 damage to all minions. &\r\n\r\n";
+
+            Assert.AreEqual(expected,result);
+        }
+    }
+}
