f2a1efc7f924ffea162df5886bf2a3dede3a55d7
Xesyto <s.oudenaarden@gmail.com>
2016-05-02 19:28:00 +0200

    Represent newlines 'nix style since that is the platform the RNN runs on.

--- Buzzbox/Buzzbox/Encode.cs
+++ Buzzbox/Buzzbox/Encode.cs
@@ -19,7 +19,7 @@ namespace Buzzbox
 
             foreach (var card in cardCollection.Cards)
             {
-                var cardOutput = $"{encoder.EncodeCard(card)}\n\n";
+                var cardOutput = $"{encoder.EncodeCard(card)}\r\n\r\n";
                 outputBuilder.Append(cardOutput);
             }
 
