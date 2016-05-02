4cd11792ca833a9905f38472da334dc46c29c26e
Xesyto <s.oudenaarden@gmail.com>
2016-05-02 18:08:53 +0200

    Added Spell and Weapon encoding tests.

--- Buzzbox/Encode-Tests/Encoders/scfdivineFormatEncoderTests.cs
+++ Buzzbox/Encode-Tests/Encoders/scfdivineFormatEncoderTests.cs
@@ -18,6 +18,28 @@ namespace Buzzbox.Encoders.Tests
             Text = "<b>Divine Shield</b>. <b>Taunt</b>. <b>Deathrattle:</b> Equip a 5/3 Ashbringer."
         };
 
+        Card testSpell = new Card
+        {
+            Name = "Fireball",
+            Type = "SPELL",
+            PlayerClass = "MAGE",
+            Rarity = "FREE",
+            Cost = 4,
+            Text = "Deal $6 damage."
+        };
+
+        Card testWeapon = new Card
+        {
+            Name = "Death's Bite",
+            Type = "WEAPON",
+            PlayerClass = "WARRIOR",
+            Rarity = "COMMON",
+            Cost = 4,
+            Attack = 4,
+            Durability = 2,
+            Text = "<b>Deathrattle:</b> Deal 1 damage to all minions."
+        };
+
         [TestMethod()]
         public void EncodeMinionCardTest()
         {
@@ -26,6 +48,31 @@ namespace Buzzbox.Encoders.Tests
             var output = encoder.EncodeCard(testMinion);
             var expectedOutput =
                 "Tirion Fordring @ Paladin |  | Minion | L | 8 | 6/6 || $DV$. $T$. $DR$ Equip a 5/3 Ashbringer. &";
+
+            Assert.AreEqual(expectedOutput, output);
+        }
+
+        [TestMethod()]
+        public void EncodeSpellCardTest()
+        {
+            var encoder = new scfdivineFormatEncoder();
+
+            var output = encoder.EncodeCard(testSpell);
+            var expectedOutput =
+                "Fireball @ Mage | Spell | C | 4 || Deal $6 damage. &";
+
+            Assert.AreEqual(expectedOutput, output);
+        }
+
+
+        [TestMethod()]
+        public void EncodeWeaponCardTest()
+        {
+            var encoder = new scfdivineFormatEncoder();
+
+            var output = encoder.EncodeCard(testWeapon);
+            var expectedOutput =
+                "Death's Bite @ Warrior | Weapon | C | 4 | 2/4 || $DR$ Deal 1 damage to all minions. &";
 
             Assert.AreEqual(expectedOutput, output);
         }
