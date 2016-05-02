7c833f6b0102faa5ac696eaec3df8089e5e6e832
Xesyto <s.oudenaarden@gmail.com>
2016-05-02 17:18:07 +0200

    Added a collections class to common. Added a dictionary for keyword replacements.

---
+++ Buzzbox/Buzzbox-Common/Collections.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace Buzzbox_Common
+{
+    class Collections
+    {
+        public Dictionary<string, string> KeywordReplacements = new Dictionary<string, string>
+        {            
+            {"Battlecry", "$B$"},
+            {"Charge", "$C$"},
+            {"Choose One", "$CO$"},
+            {"Combo", "$C$"},
+            {"Counter", "$CR$"},
+            {"Deathrattle", "$DR$"},
+            {"Discover", "$D$"},           
+            {"Divine Shield", "$DV$"},
+            {"Enrage", "$E$"},
+            {"Freeze", "$F$"},
+            {"Immune", "$I$"},
+            {"Inspire", "$IN$"},            
+            {"Overload", "$O$"},
+            {"Secret", "$S$"},
+            {"Silence", "$SI$"},
+            {"Stealth", "$ST$"},
+            {"Spell Damage", "$SD$"},
+            {"Summon", "$SU$"},
+            {"Taunt", "$T$"},
+            {"Transform", "$TR$"},
+            {"Windfury", "$W$"}
+        };
+    }
+}
