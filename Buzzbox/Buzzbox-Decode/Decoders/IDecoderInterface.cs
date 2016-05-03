fcceff327cfe4a307fa3dbff8c699753285318e3
Xesyto <s.oudenaarden@gmail.com>
2016-05-03 13:51:22 +0200

    Added Decoder Interface and stub scfdivine Decoder.

---
+++ Buzzbox/Buzzbox-Decode/Decoders/IDecoderInterface.cs
@@ -0,0 +1,9 @@
+﻿using Buzzbox_Common;
+
+namespace Buzzbox_Decode.Decoders
+{
+    public interface IDecoderInterface
+    {
+        Card DecodeCard(string cardLine);
+    }
+}
---
+++ Buzzbox/Buzzbox-Decode/Decoders/scfdivineFormatDecoder.cs
@@ -0,0 +1,12 @@
+﻿using Buzzbox_Common;
+
+namespace Buzzbox_Decode.Decoders
+{
+    public class scfdivineFormatDecoder : IDecoderInterface
+    {
+        public Card DecodeCard(string cardLine)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
