3e7419ca790e3116433211e90ec4ed86375eecf8
Xesyto <s.oudenaarden@gmail.com>
2016-05-03 18:39:53 +0200

    Test improvements.

--- Buzzbox/Decode-Tests/Decoders/scfdivineFormatDecoderTests.cs
+++ Buzzbox/Decode-Tests/Decoders/scfdivineFormatDecoderTests.cs
@@ -6,7 +6,7 @@ namespace Buzzbox_Decode.Decoders.Tests
     [TestClass()]
     public class scfdivineFormatDecoderTests
     {
-        Card testMinion = new Card
+        private Card testMinion = new Card
         {
             Name = "Tirion Fordring",
             Type = "MINION",
@@ -18,7 +18,7 @@ namespace Buzzbox_Decode.Decoders.Tests
             Text = "<b>Divine Shield</b>. <b>Taunt</b>. <b>Deathrattle:</b> Equip a 5/3 Ashbringer."
         };
 
-        Card testSpell = new Card
+        private Card testSpell = new Card
         {
             Name = "Fireball",
             Type = "SPELL",
@@ -28,7 +28,7 @@ namespace Buzzbox_Decode.Decoders.Tests
             Text = "Deal $6 damage."
         };
 
-        Card testWeapon = new Card
+        private Card testWeapon = new Card
         {
             Name = "Death's Bite",
             Type = "WEAPON",
@@ -44,7 +44,8 @@ namespace Buzzbox_Decode.Decoders.Tests
         public void DecodeMinionCardTest()
         {
             var decoder = new scfdivineFormatDecoder();
-            var input = "Tirion Fordring @ Paladin |  | Minion | L | 8 | 6/6 || $DV$. $T$. $DR$ Equip a 5/3 Ashbringer. &";
+            var input =
+                "Tirion Fordring @ Paladin |  | Minion | L | 8 | 6/6 || $DV$. $T$. $DR$ Equip a 5/3 Ashbringer. &";
 
             var output = decoder.DecodeCard(input);
 
