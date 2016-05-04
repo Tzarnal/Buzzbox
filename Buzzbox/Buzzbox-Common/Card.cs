816951fa2ea86a6e8ae176323acb6b9570c8b394
Xesyto <s.oudenaarden@gmail.com>
2016-05-04 16:38:38 +0200

    Added a ToString to cards, displays a short text version of a card. Mostly for use on console.

--- Buzzbox/Buzzbox-Common/Card.cs
+++ Buzzbox/Buzzbox-Common/Card.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 
 namespace Buzzbox_Common
@@ -26,5 +27,48 @@ namespace Buzzbox_Common
         public string PlayerClass { get; set; }
         public int? Durability { get; set; }
         public string Source { get; set; }
+
+        public override string ToString()
+        {
+            switch (Type)
+            {
+                case "MINION":
+                    return DisplayMinion();
+                
+                case "SPELL":
+                    return DisplaySpell();
+
+                case "WEAPON":
+                    return DisplayWeapon();
+
+                default:
+                    return base.ToString();
+            }            
+        }
+
+        private string DisplayMinion()
+        {
+            var text = Text.RemoveMarkup();
+            text = Regex.Replace(text, @"\r\n?|\n", " ");
+
+            return $"{(PlayerClass ?? "Neutral").CapitalizeOnlyFirstLetter()} {(Race ?? "")} Minion: {Name} - {Attack}/{Health} for {Cost} - {text}";
+        }
+
+        private string DisplaySpell()
+        {
+            var text = Text.RemoveMarkup();
+            text = Regex.Replace(text, @"\r\n?|\n", " ");
+
+            return $"{(PlayerClass ?? "Neutral").CapitalizeOnlyFirstLetter()} Spell: {Name} - {Cost} mana - {text}";
+        }
+
+        private string DisplayWeapon()
+        {
+            var text = Text.RemoveMarkup();
+            text = Regex.Replace(text, @"\r\n?|\n", " ");
+
+            return $"{(PlayerClass ?? "Neutral").CapitalizeOnlyFirstLetter()} Weapon: {Name} - {Attack}/{Durability} for {Cost} - {text}";
+        }
     }
+    
 }
