47deec960dfd592a213f8a113c007e5f60783731
Xesyto <s.oudenaarden@gmail.com>
2016-05-08 12:28:52 +0200

    Added tests for MtgEncoder Format Decoding.

--- Buzzbox/Decode-Tests/DecodeTests.cs
+++ Buzzbox/Decode-Tests/DecodeTests.cs
@@ -2,7 +2,6 @@
 using Buzzbox_Common;
 using System;
 
-
 namespace Buzzbox_Decode.Tests
 {
     [TestClass()]
@@ -12,10 +11,25 @@ namespace Buzzbox_Decode.Tests
         [TestMethod()]
         public void DecodeStringscfdivineFormatTest()
         {
-            var collection = new CardCollection();
-
             var decoder = new Decode();
             var result = decoder.DecodeString("Tirion Fordring @ Paladin |  | Minion | L | 8 | 6/6 || $DV$. $T$. $DR$ Equip a 5/3 Ashbringer. &\n\nFireball @ Mage | Spell | C | 4 || Deal $6 damage. &\n\nDeath's Bite @ Warrior | Weapon | C | 4 | 4/2 || $DR$ Deal 1 damage to all minions. &\n\n",EncodingFormats.scfdivineFormat);
+
+            var expected = "Paladin Neutral Minion: Tirion Fordring - 6/6 for 8 - Divine Shield. Taunt. Deathrattle Equip a 5/3 Ashbringer.\r\nMage Spell: Fireball - 4 mana - Deal $6 damage.\r\nWarrior Weapon: Death's Bite - 4/2 for 4 - Deathrattle Deal 1 damage to all minions.\r\n";
+
+            var resultString = "";
+            foreach (var card in result.Cards)
+            {
+                resultString += card.ToString() + Environment.NewLine;
+            }
+
+            Assert.AreEqual(expected, resultString);
+        }
+
+        [TestMethod()]
+        public void DecodeStringMtgEncodeFormatTest()
+        {
+            var decoder = new Decode();
+            var result = decoder.DecodeString("|3minion|4paladin|5none|6legendary|7&^^^^^^^^|8&^^^^^^|9&^^^^^^|2$DV$. $T$. $DR$: equip a &^^^^^/&^^^ ashbringer.|1tirion fordring|\n\n|3spell|4mage|6common|7&^^^^|2deal $&^^^^^^ damage.|1fireball|\n\n|3weapon|4warrior|6common|7&^^^^|8&^^^^|9&^^|2$DR$: deal &^ damage to all minions.|1death's bite|\n\n", EncodingFormats.MtgFormat);
 
             var expected = "Paladin Neutral Minion: Tirion Fordring - 6/6 for 8 - Divine Shield. Taunt. Deathrattle Equip a 5/3 Ashbringer.\r\nMage Spell: Fireball - 4 mana - Deal $6 damage.\r\nWarrior Weapon: Death's Bite - 4/2 for 4 - Deathrattle Deal 1 damage to all minions.\r\n";
 
---
+++ Buzzbox/Decode-Tests/Decoders/MtgEncodeFormatDecoderTests.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Buzzbox_Common;
+
+namespace Buzzbox_Decode.Decoders.Tests
+{
+    [TestClass()]
+    public class MtgEncodeFormatDecoderTests
+    {
+        private Card testMinion = new Card
+        {
+            Name = "Tirion Fordring",
+            Type = "MINION",
+            PlayerClass = "PALADIN",
+            Rarity = "LEGENDARY",
+            Cost = 8,
+            Attack = 6,
+            Health = 6,
+            Text = "<b>Divine Shield</b>. <b>Taunt</b>. <b>Deathrattle</b>: Equip a 5/3 Ashbringer."
+        };
+
+        private Card testSpell = new Card
+        {
+            Name = "Fireball",
+            Type = "SPELL",
+            PlayerClass = "MAGE",
+            Rarity = "COMMON",
+            Cost = 4,
+            Text = "Deal $6 damage."
+        };
+
+        private Card testWeapon = new Card
+        {
+            Name = "Death's Bite",
+            Type = "WEAPON",
+            PlayerClass = "WARRIOR",
+            Rarity = "COMMON",
+            Cost = 4,
+            Attack = 4,
+            Durability = 2,
+            Text = "<b>Deathrattle</b>: Deal 1 damage to all minions."
+        };
+
+        [TestMethod()]
+        public void DecodeMinionCardTest()
+        {
+            var decoder = new MtgEncodeFormatDecoder();
+            var input =
+                "|3minion|4paladin|5none|6legendary|7&^^^^^^^^|8&^^^^^^|9&^^^^^^|2$DV$. $T$. $DR$: equip a &^^^^^/&^^^ ashbringer.|1tirion fordring|";
+
+            var output = decoder.DecodeCard(input);
+
+            Assert.AreEqual(testMinion.Name, output.Name);
+            Assert.AreEqual(testMinion.Type, output.Type);
+            Assert.AreEqual(testMinion.PlayerClass, output.PlayerClass);
+            Assert.AreEqual(testMinion.Rarity, output.Rarity);
+            Assert.AreEqual(testMinion.Cost, output.Cost);
+            Assert.AreEqual(testMinion.Attack, output.Attack);
+            Assert.AreEqual(testMinion.Health, output.Health);
+            Assert.AreEqual("<b>Divine Shield</b>. <b>Taunt</b>. <b>Deathrattle</b>: Equip a 5/3 ashbringer.", output.Text); //Exception for Ashbringer because detecting Proper Nouns is rather hard.
+
+        }
+
+        [TestMethod()]
+        public void DecodeSpellCardTest()
+        {
+            var decoder = new MtgEncodeFormatDecoder();
+            var input = "|3spell|4mage|6common|7&^^^^|2deal $&^^^^^^ damage.|1fireball|";
+
+            var output = decoder.DecodeCard(input);
+
+            Assert.AreEqual(testSpell.Name, output.Name);
+            Assert.AreEqual(testSpell.Type, output.Type);
+            Assert.AreEqual(testSpell.PlayerClass, output.PlayerClass);
+            Assert.AreEqual(testSpell.Rarity, output.Rarity);
+            Assert.AreEqual(testSpell.Cost, output.Cost);
+            Assert.AreEqual(testSpell.Text, output.Text);
+        }
+
+        [TestMethod()]
+        public void DecodeWeaponCardTest()
+        {
+            var decoder = new MtgEncodeFormatDecoder();
+            var input = "|3weapon|4warrior|6common|7&^^^^|8&^^^^|9&^^|2$DR$: deal &^ damage to all minions.|1death's bite|";
+
+            var output = decoder.DecodeCard(input);
+
+            Assert.AreEqual(testWeapon.Name, output.Name);
+            Assert.AreEqual(testWeapon.Type, output.Type);
+            Assert.AreEqual(testWeapon.PlayerClass, output.PlayerClass);
+            Assert.AreEqual(testWeapon.Rarity, output.Rarity);
+            Assert.AreEqual(testWeapon.Cost, output.Cost);
+            Assert.AreEqual(testWeapon.Attack, output.Attack);
+            Assert.AreEqual(testWeapon.Durability, output.Durability);
+            Assert.AreEqual(testWeapon.Text, output.Text);
+        }
+
+    }
+}
