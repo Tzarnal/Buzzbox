e99b1a7bcbd4c9a1fe221af26202887ae42e9168
Xesyto <s.oudenaarden@gmail.com>
2016-05-08 15:16:18 +0200

    Added MtgEncode format decoder as default option od Buzzbox-Decode

--- Buzzbox/Buzzbox-Decode/Program.cs
+++ Buzzbox/Buzzbox-Decode/Program.cs
@@ -23,7 +23,7 @@ namespace Buzzbox_Decode
 
             [Option('e', "encoding",
                 HelpText = "Which format to decode from.",
-                DefaultValue = EncodingFormats.scfdivineFormat)]
+                DefaultValue = EncodingFormats.MtgFormat)]
             public EncodingFormats EncodingFormat { get; set; }
 
             [Option("verbose",
