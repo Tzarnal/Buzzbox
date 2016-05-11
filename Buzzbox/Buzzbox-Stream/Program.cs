160930315c2d8b6370ae59a733e7162273adaafc
Xesyto <s.oudenaarden@gmail.com>
2016-05-11 11:53:35 +0200

    Added proof of concept Buzzbox-Stream to work with the new -stream_cmd in mtg torch-rnn.

---
+++ Buzzbox/Buzzbox-Stream/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using Buzzbox_Common;
+using CommandLine;
+using CommandLine.Text;
+
+namespace Buzzbox_Stream
+{
+    class Program
+    {
+        private class Options
+        {
+            [Option('i', "input",
+                Required = true,
+                HelpText = "Path to input file to be Encoded, must be in hearthstonejson format.")]
+            public string InputFile { get; set; }
+
+            [ValueList(typeof(List<string>))]
+            public IList<string> FdsList { get; set; }
+
+            [Option("loop-forever", DefaultValue = false,
+                HelpText = "When the end of a card set is reached, shuffle it and start again.")]
+            public bool LoopForever { get; set; }
+
+            [Option("shuffle-fields", DefaultValue = false,
+                HelpText = "Shuffles the fields of the output.")]
+            public bool ShuffleFields { get; set; }
+
+            [HelpOption]
+            public string GetUsage()
+            {
+                return "Streams encoded card lines to torch-rss" +
+                    HelpText.AutoBuild(this,
+                  (current) => HelpText.DefaultParsingErrorsHandler(this, current));
+            }
+        }
+
+
+        private static void Main(string[] args)
+        {
+            var options = new Options();
+            var commandLineResults = Parser.Default.ParseArguments(args, options);
+            
+
+            if (commandLineResults)
+            {
+                List<StreamWriter> streams = new List<StreamWriter>();
+                var countdownEvent = new CountdownEvent(options.FdsList.Count);
+                string inPath;
+
+                //Use Path to get proper filesystem path for input
+                try
+                {
+                    inPath = Path.GetFullPath(options.InputFile);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Invalid Path to input file: {0}", options.InputFile);
+                    return;
+                }
+                
+                //Check if input file is real.
+                if (!File.Exists(inPath))
+                {
+                    Console.WriteLine("File does not exist: {0}", options.InputFile);
+                    return;
+                }
+
+                foreach (var fd in options.FdsList)
+                {
+                    //Console.WriteLine(fd);
+                    var fileLoc = "/proc/self/fd/" + fd;
+                    //var fileLoc = "file" + fd + ".txt";
+                    var stream = new StreamWriter(fileLoc);
+
+                    //Load card collection to encode
+                    CardCollection cardCollection;
+                    try
+                    {
+                        cardCollection = CardCollection.Load(inPath);
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("Could not parse '{0}'.", inPath);
+                        return;
+                    }
+
+                    var streamEncoder = new StreamEncode(cardCollection,stream)
+                    {
+                        LoopForever = options.LoopForever,
+                        ShuffleFields = options.ShuffleFields
+                    };
+
+                    new Thread(delegate ()
+                    {
+                        streamEncoder.ThreadEntry();
+                        countdownEvent.Signal();
+                    }).Start();
+                }
+
+                countdownEvent.Wait();
+                Console.WriteLine("Final thread finished writing. Buzzbox-Stream exiting.");
+            }
+        }
+    }
+}
---
+++ Buzzbox/Buzzbox-Stream/StreamEncode.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Buzzbox_Common;
+using Buzzbox_Common.Encoders;
+
+namespace Buzzbox_Stream
+{
+    class StreamEncode
+    {
+        public bool LoopForever;
+        public bool ShuffleFields;
+
+        private MtgEncodeFormatEncoder _encoder;        
+        private CardCollection _cardCollection;
+        private StreamWriter _stream;
+
+
+
+        public StreamEncode(CardCollection cardCollection, StreamWriter stream)
+        {
+            _encoder = new MtgEncodeFormatEncoder();
+
+            _cardCollection = cardCollection;
+            _stream = stream;
+        }
+
+        public void ThreadEntry()
+        {
+            do
+            {
+                _cardCollection.Cards.Shuffle();
+
+                foreach (var card in _cardCollection.Cards)
+                {
+                    var outputLine = _encoder.EncodeCard(card) + "\n\n";
+
+                    if (ShuffleFields)
+                    {
+                        outputLine = ShuffleCardFields(outputLine);
+                    }
+
+                    //actually output
+                    _stream.Write(outputLine);
+                }
+               
+            } while (LoopForever);
+
+            _stream.Close();
+        }
+
+        private string ShuffleCardFields(string cardLine)
+        {
+            cardLine = cardLine.TrimEnd('|').TrimStart('|');
+            List<string> fields = cardLine.Split('|').ToList();
+            fields.Shuffle();
+            return $"|{string.Join("|", fields)}|";
+        }
+    }
+}
