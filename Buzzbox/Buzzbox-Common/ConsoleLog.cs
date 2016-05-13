217c3f6a698470c93bace9b52c05e6b3b43bb3e3
Xesyto <s.oudenaarden@gmail.com>
2016-05-13 17:34:19 +0200

    Added a singleton ConsoleLog class to common to handle console output at various levels of verbosity.

---
+++ Buzzbox/Buzzbox-Common/ConsoleLog.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Buzzbox_Common
+{
+    //Write things to the console based on what level of information the user wants to see.
+    public sealed class ConsoleLog
+    {
+        public bool Verbose;
+        public bool Silent;
+
+        private static readonly ConsoleLog instance = new ConsoleLog();
+
+        static ConsoleLog()
+        {
+        }
+
+        private ConsoleLog()
+        {
+        }
+
+        public void VerboseWriteLine(string text)
+        {
+            if(!Silent && Verbose)
+                Console.WriteLine(text);
+        }
+
+        public void WriteLine(string text)
+        {
+            if(!Silent)
+                Console.WriteLine(text);
+        }
+
+        public static ConsoleLog Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+    }
+}
